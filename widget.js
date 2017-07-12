900acb5845f743a460917a2f3d0d8fae2bdfaabd
Patrick McDermott <patrick.mcdermott@libiquity.com>
2017-07-12 15:08:49 -0400

    widget.js: Set domain=.libiquity.com
    Otherwise, the widget appears twice for users who browse both
    www.libiquity.com and shop.libiquity.com.

--- widget.js
+++ widget.js
@@ -121,7 +121,7 @@
       d.setTime(d.getTime()+(exdays*24*60*60*1000));
 
       var expires = "expires="+d.toGMTString();
-      document.cookie = name + "=" + val + "; " + expires + "; path=/";
+      document.cookie = name + "=" + val + "; " + expires + "; domain=.libiquity.com; path=/";
     },
 
     getCookie: function(cname) {
